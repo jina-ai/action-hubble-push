06740218464862978b97850e71383fbf5411957a
Nan Wang <nan.wang@jina.ai>
2021-09-20 14:13:53 +0800

    feat: first initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# Push Executors to Hubble
+
+Push the executor to Hubble with the tags `$GIT_TAG` and `latest`
---
+++ push.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -ex
+
+sudo apt-get update && sudo apt-get install -y jq curl
+
+JINA_VERSION=$(curl -L -s "https://pypi.org/pypi/jina/json" \
+  |  jq  -r '.releases | keys | .[]
+    | select(contains("dev") | not)
+    | select(startswith("2."))' \
+  | sort -V | tail -1)
+pip install git+https://github.com/jina-ai/jina.git@v${JINA_VERSION}#egg=jina[standard]
+
+GIT_TAG=$1
+PUSH_DIR=$2
+
+exit_code=1
+# empty change is detected as home directory
+if [ -z "$PUSH_DIR" ]
+then
+      echo "\$PUSH_DIR is empty"
+      exit_code=0
+      exit $exit_code
+fi
+
+echo pushing $PUSH_DIR
+cd $PUSH_DIR
+
+pip install yq
+
+exec_name=`yq -r .name manifest.yml`
+echo executor name is $exec_name
+
+version=`jina -vf`
+echo jina version $version
+
+# clone file with secrets
+echo "::add-mask::$token"
+curl -H "Authorization: token $token" -H 'Accept: application/vnd.github.v3.raw' -O https://api.github.com/repos/jina-ai/executors-secrets/contents/secrets.json
+
+exec_uuid=`cat secrets.json | jq -r '.[] | select(.Alias=="'$exec_name'").UUID8'`
+echo "::add-mask::$exec_uuid"
+echo UUID=`head -c 3 <(echo $exec_uuid)`
+
+exec_secret=`cat secrets.json | jq -r '.[] | select(.Alias=="'$exec_name'").Secret'`
+echo "::add-mask::$exec_secret"
+echo SECRET=`head -c 3 <(echo $exec_secret)`
+
+rm secrets.json
+
+# we only push to a tag once,
+# if it doesn't exist
+echo git tag = $GIT_TAG
+
+if [ -z "$GIT_TAG" ]
+then
+  echo WARNING, no git tag!
+else
+  echo git tag = $GIT_TAG
+  jina hub pull jinahub+docker://$exec_name/$GIT_TAG
+  exists=$?
+  if [[ $exists == 1 ]]; then
+    echo does NOT exist, pushing to latest and $GIT_TAG
+    jina hub push --force $exec_uuid --secret $exec_secret . -t $GIT_TAG -t latest
+  else
+    echo exists, only push to latest
+    jina hub push --force $exec_uuid --secret $exec_secret .
+  fi
+fi
+
+exit_code=0
+exit $exit_code
