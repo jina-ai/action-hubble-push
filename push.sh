8560e7998d15a25d389c1be0f35187dbebef0205
Nan Wang <nan.wang@jina.ai>
2021-09-20 15:40:19 +0800

    feat: add pushing gpu tag

--- push.sh
+++ push.sh
@@ -10,6 +10,7 @@ pip install git+https://github.com/jina-ai/jina.git@v${JINA_VERSION}#egg=jina[st
 
 GIT_TAG=$1
 PUSH_DIR=$2
+DOCKERFILE_GPU=Dockerfile.gpu
 
 exit_code=1
 # empty change is detected as home directory
@@ -41,6 +42,7 @@ echo SECRET=`head -c 3 <(echo $exec_secret)`
 # if it doesn't exist
 echo git tag = $GIT_TAG
 
+# push cpu version
 if [ -z "$GIT_TAG" ]
 then
   echo WARNING, no git tag!
@@ -54,6 +56,27 @@ else
   else
     echo exists, only push to latest
     jina hub push --force $exec_uuid --secret $exec_secret .
+  fi
+fi
+
+# push gpu version
+GIT_TAG_GPU=$GIT_TAG-gpu
+if [ -z "$GIT_TAG" ]
+then
+  echo WARNING, no git tag!
+elif [ -f $DOCKERFILE_GPU ]
+then
+  echo no $DOCKERFILE_GPU, skip pushing the gpu version
+else
+  echo git gpu tag = $GIT_TAG_GPU
+  jina hub pull jinahub+docker://$exec_name/$GIT_TAG_GPU
+  exists=$?
+  if [[ $exists == 1 ]]; then
+    echo does NOT exist, pushing to latest and $GIT_TAG_GPU
+    jina hub push --force $exec_uuid --secret $exec_secret -t $GIT_TAG_GPU -t latest-gpu -f $DOCKERFILE_GPU .
+  else
+    echo exists, only push to latest-gpu
+    jina hub push --force $exec_uuid --secret $exec_secret -t latest-gpu -f $DOCKERFILE_GPU .
   fi
 fi
 
