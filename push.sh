56a764574576a539793d4c6c07a415a46c1af05d
Delgermurun <delgermurun@users.noreply.github.com>
2022-06-08 15:51:39 +0200

    feat: use auth token (#6)

--- push.sh
+++ push.sh
@@ -26,13 +26,21 @@ echo NAME=$exec_name
 
 if [ -z "$exec_secret" ]
 then
-  echo no secret provided. Add the secret to your repo
-  exit_code=1
-  exit 1
+  echo no executor secret provided
+  exec_secret="dummy-secret"
+fi
+
+if [ -z "$jinahub_token" ]
+then
+  echo no jinahub token provided
+  jinahub_token="dummy-token"
 fi
 
 echo "::add-mask::$exec_secret"
 echo SECRET=`head -c 3 <(echo $exec_secret)`
+
+echo "::add-mask::$jinahub_token"
+echo JINA_AUTH_TOKEN=`head -c 3 <(echo $jinahub_token)`
 
 JINA_VERSION=$(curl -L -s "https://pypi.org/pypi/jina/json" \
   |  jq  -r '.releases | keys | .[]
@@ -57,7 +65,7 @@ else
   exists=$?
   if [[ $exists == 1 ]]; then
     echo does NOT exist, pushing to latest and $GIT_TAG
-    jina hub push --force $exec_name --secret $exec_secret . -t $GIT_TAG -t latest
+    JINA_AUTH_TOKEN=$jinahub_token jina hub push --force $exec_name --secret $exec_secret . -t $GIT_TAG -t latest
     push_success=$?
     if [[ $push_success != 0 ]]; then
       echo push failed. Check error
@@ -66,7 +74,7 @@ else
     fi
   else
     echo exists, only push to latest
-    jina hub push --force $exec_name --secret $exec_secret .
+    JINA_AUTH_TOKEN=$jinahub_token jina hub push --force $exec_name --secret $exec_secret .
     push_success=$?
     if [[ $push_success != 0 ]]; then
       echo push failed. Check error
@@ -88,7 +96,7 @@ then
   exists=$?
   if [[ $exists == 1 ]]; then
     echo does NOT exist, pushing to latest and $GIT_TAG_GPU
-    jina hub push --force $exec_name --secret $exec_secret -t $GIT_TAG_GPU -t latest-gpu -f $DOCKERFILE_GPU .
+    JINA_AUTH_TOKEN=$jinahub_token jina hub push --force $exec_name --secret $exec_secret -t $GIT_TAG_GPU -t latest-gpu -f $DOCKERFILE_GPU .
     push_success=$?
     if [[ $push_success != 0 ]]; then
       echo push failed. Check error
@@ -97,7 +105,7 @@ then
     fi
   else
     echo exists, only push to latest-gpu
-    jina hub push --force $exec_name --secret $exec_secret -t latest-gpu -f $DOCKERFILE_GPU .
+    JINA_AUTH_TOKEN=$jinahub_token jina hub push --force $exec_name --secret $exec_secret -t latest-gpu -f $DOCKERFILE_GPU .
     push_success=$?
     if [[ $push_success != 0 ]]; then
       echo push failed. Check error
